dd7010e712641333a767c0d75e0cebe78134397d
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-09-26 12:45:29 +0200

    Part 4 done. A bit of scruffing with ES5 :D.

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-var colors = [
-    "rgb(255, 55, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(255, 255, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 // Taking our variables
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
-
 var colorDisplay = document.getElementById("color-display");
 var message = document.querySelector('#message')
+var h1 = document.querySelector("h1")
 
 colorDisplay.textContent = pickedColor;
 
-// Function for changing collors
+// Function for changing colors
 function changeColors(color) {
     for(var i = 0; i< squares.length; i++) {
         // change colors for for given color
@@ -24,9 +17,31 @@ function changeColors(color) {
     }
 }
 
+// Create colorpicker
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random]
+}
+
+function generateRandomColors(num) {
+    // Make array
+    var arr = []
+    // Adding random colors to array
+    for(var i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    // Return value
+    return arr
+}
+
+function randomColor() {
+    // Pick a "red" from 0-255
+    var red = Math.floor(Math.random() * 256);
+    // Pick a "green" from 0-255
+    var green = Math.floor(Math.random() * 256);
+    // Pick a "blue" from 0-255
+    var blue = Math.floor(Math.random() * 256);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 // Creating the grid with different collors
@@ -38,11 +53,12 @@ for(var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
         var clickedColor = this.style.backgroundColor
         if(clickedColor === pickedColor) {
-            message.textContent = "Correct"
+            message.textContent = "Correct!"
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor
         } else {
             this.style.backgroundColor = "black"
-            message.textContent = "Try again"
+            message.textContent = "Try again!"
         }
     });
 }
