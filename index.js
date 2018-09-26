02c5fe455d9f0d91423c36005070c2632a6103b1
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-09-26 12:33:30 +0200

    Part 3 done

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ var colors = [
     "rgb(0, 0, 255)"
 ]
 
+// Taking our variables
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3]
+var pickedColor = pickColor();
 
 var colorDisplay = document.getElementById("color-display");
+var message = document.querySelector('#message')
 
 colorDisplay.textContent = pickedColor;
 
+// Function for changing collors
+function changeColors(color) {
+    for(var i = 0; i< squares.length; i++) {
+        // change colors for for given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random]
+}
+
+// Creating the grid with different collors
 for(var i = 0; i < squares.length; i++) {
     // Add initial color to squares
     squares[i].style.backgroundColor = colors[i]
@@ -22,11 +38,11 @@ for(var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
         var clickedColor = this.style.backgroundColor
         if(clickedColor === pickedColor) {
-            alert("correct");
+            message.textContent = "Correct"
+            changeColors(clickedColor);
         } else {
-            alert("Wrong");
+            this.style.backgroundColor = "black"
+            message.textContent = "Try again"
         }
     });
 }
-
-
