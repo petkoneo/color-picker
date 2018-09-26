b94abe283513cbe6fb2ba8742d8b441d1ce1386d
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-09-26 14:06:16 +0200

    Finalized project

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
-var colors = generateRandomColors(6);
-
-// Taking our variables
+var numberSquares = 6;
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("color-display");
-var message = document.querySelector('#message')
-var h1 = document.querySelector("h1")
+var message = document.querySelector('#message');
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
 
-colorDisplay.textContent = pickedColor;
+
+init();
+
+function init() {
+    // Modebutton setup
+    for(var i = 0; i < modeButtons.length; i++){
+        modeButtons[i].addEventListener("click", function(){
+            for(var j = 0; j < modeButtons.length; j++){
+                modeButtons[j].classList.remove("selected");
+            }
+            this.classList.add("selected");
+
+            // Figure out how many squares shows up
+            this.textContent === "Easy" ? numberSquares = 3 : numberSquares = 6;
+            reset();
+        });
+    }
+
+    // Creating the grid with different collors
+    for(var i = 0; i < squares.length; i++) {
+
+        // Add clicklistener to squares
+        squares[i].addEventListener("click", function () {
+            var clickedColor = this.style.backgroundColor
+            if(clickedColor === pickedColor) {
+                message.textContent = "Correct!"
+                changeColors(clickedColor);
+                h1.style.backgroundColor = clickedColor
+                resetButton.textContent = "Play a new Game?"
+            } else {
+                this.style.backgroundColor = "black"
+                message.textContent = "Try again!"
+            }
+        });
+    }
+
+    reset();
+}
+
+
+
+function reset() {
+    // Generate new random color
+    colors = generateRandomColors(numberSquares);
+    // Generate color randomly from Array
+    pickedColor = pickColor();
+    // Change colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    // Deleting middle text
+    message.textContent = "";
+    // Change colors of squares
+    for(var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i]
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    h1.style.backgroundColor = "steelblue"
+    resetButton.textContent = "New Colors"
+}
+
+resetButton.addEventListener("click", function(){
+    reset();
+});
 
 // Function for changing colors
 function changeColors(color) {
@@ -44,21 +110,3 @@ function randomColor() {
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
-// Creating the grid with different collors
-for(var i = 0; i < squares.length; i++) {
-    // Add initial color to squares
-    squares[i].style.backgroundColor = colors[i]
-
-    // Add clicklistener to squares
-    squares[i].addEventListener("click", function () {
-        var clickedColor = this.style.backgroundColor
-        if(clickedColor === pickedColor) {
-            message.textContent = "Correct!"
-            changeColors(clickedColor);
-            h1.style.backgroundColor = clickedColor
-        } else {
-            this.style.backgroundColor = "black"
-            message.textContent = "Try again!"
-        }
-    });
-}
